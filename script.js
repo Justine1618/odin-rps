d5c7b6ccfa3c3787b60ac64ad26d15c2aeebf646
Justine Craig <justincrg@gmail.com>
2022-01-11 09:11:21 -0800

    Added basic functions to play rps with computer

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
-function computerPlay() {
+function getComputerGuess() {
     let randChoice = Math.random();
     if (randChoice < 0.33) {
-        return "Rock";
+        return "rock";
     }
     else if (randChoice < 0.66) {
-        return "Paper";
+        return "paper";
     }
-    else return "Scissors";
+    else return "scissors";
+}
+
+function getPlayerGuess() {
+    let playerGuess = prompt("Please guess rock, paper, or scissors");
+    //Returns player guess in cleaned up form, all lower case
+    //Will assume that player entered a valid choice, no validation required
+    return playerGuess.trim().toLowerCase();
+}
+
+function playRound(playerGuess, computerGuess) {
+    if(playerGuess == computerGuess) {
+        return "Draw";
+    }
+    else if (playerGuess == "rock" && computerGuess == "scissors") {
+        return "Player Wins";
+    }
+    else if (playerGuess == "paper" && computerGuess == "rock") {
+        return "Player Wins";
+    }
+    else if (playerGuess == "scissors" && computerGuess == "paper") {
+        return "Player Wins";
+    }
+    else {
+        return "Computer Wins";
+    }
+}
+
+function play() {
+    for(let i = 0; i < 5; i++) {
+        const playerGuess = getPlayerGuess();
+        const computerGuess = getComputerGuess();
+        console.log(playRound(playerGuess, computerGuess));
+        
+    }
 }
